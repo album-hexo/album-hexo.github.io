ecbdf473bb8dae0492256c3dff6210e4ddba0d40
0x100815 <shtnb@outlook.com>
2024-08-11 12:07:42 +0800

    Site updated: 2024-08-11 12:07:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/06/hello-world/"];function toRandomPost(){
+var posts=["2024/08/11/try/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
