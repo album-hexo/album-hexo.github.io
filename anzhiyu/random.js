a521b88220990974d937ffe97f0ced20ed180b43
0x100815 <shtnb@outlook.com>
2024-08-15 21:27:43 +0800

    Site updated: 2024-08-15 21:27:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/15/daily_photo/","2024/08/11/try/"];function toRandomPost(){
+var posts=["2024/08/11/try/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
