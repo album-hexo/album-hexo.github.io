bc146dd22bc9dd4a59bbd27013c36fe8948022ea
0x100815 <shtnb@outlook.com>
2024-09-01 12:06:35 +0800

    Site updated: 2024-09-01 12:06:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/29/anzhiyu主题标签/"];function toRandomPost(){
+var posts=["posts/d50a.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
